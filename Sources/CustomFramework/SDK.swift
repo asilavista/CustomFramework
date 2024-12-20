5ac8581610ac547f2dacdf0d6246d7fe6d394d32
Asi <asigivati@gmail.com>
2024-12-20 23:45:05 +0200

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,12 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Asi Givati on 20/12/2024.
+//
+
+public struct SDK {
+    public static func doSomething() {
+        print("Doing some work...")
+    }
+}
