07fec774b6f8b36e406b2efadd417dcbe9fcdf7d
Asi <asigivati@gmail.com>
2024-12-21 10:59:24 +0200

    ver 0.7

--- Package.swift
+++ Package.swift
@@ -17,12 +17,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(
-//            name: "CustomFramework",
-//            path: "./Sources/CustomFramework.xcframework")
-                .target(
-                    name: "CustomFramework",
-                    path: "./Sources/CustomFramework.xcframework",
-                    resources: [.process("Assets.xcassets")])
+        .binaryTarget(
+            name: "CustomFramework",
+            path: "./Sources/CustomFramework.xcframework")
     ]
 )
